5088aee4557a2b041ced432ea89cd7e3d8c1c3b4
cbarnes3 <cbarnes1827@gmail.com>
2022-09-21 20:41:50 -0400

    Update files, Finish Project

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Calvin Barnes on 9/21/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cover: UIImageView!
+}
